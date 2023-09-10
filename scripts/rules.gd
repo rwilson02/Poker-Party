ee2269a1532a141305393657dd086f5ec130dca3
rwilson02 <78097094+rwilson02@users.noreply.github.com>
2023-09-09 22:17:39 -0400

    Store rules in JSON files
    don't gotta hardcode anything and it's easy to change later because they're just dictionaries really

--- scripts/rules.gd
+++ scripts/rules.gd
@@ -1,49 +1,9 @@
 extends Node
 
-var RULES = {
-	"SUITS": 4,
-	"VALS_PER_SUIT": 13,
-	"HOLE_CARDS": 2,
-	"COMM_CARDS": 5,
-	"CARDS_PER_HAND": 5,
-	"HAND_RANKS": {
-		# Base hands
-		"5K": 900, # 5-of-a-kind
-		"SF": 800, # Straight flush
-		"4K": 700, # 4-of-a-kind
-		"FH": 600, # Full house
-		"FL": 500, # Flush
-		"ST": 400, # Straight
-		"3K": 300, # 3-of-a-kind
-		"2P": 200, # 2 pair
-		"1P": 100, # 1 pair
-		"HC": 000, # High card
-		"": -9999, # Junk value
-	}
-}
+var RULES
 
-var BASE_HAND_RANKS = {
-	"5K": 900,
-	"SF": 800,
-	"4K": 700,
-	"FH": 600,
-	"FL": 500,
-	"ST": 400,
-	"3K": 300,
-	"2P": 200,
-	"1P": 100,
-	"HC": 000,
-	"": -9999,
-	}
-
-var SIX_HAND_RANKS = {
-	# 6-card exclusives (values estimated)
-	"FH": 750, # Full house (4-2), ranks higher than 4-of-a-kind
-	"CR": 550, # Crowd (not-quite-full house)
-	"3P": 570, # 3 pair
-	"2T": 590, # 2 trips
-	"6K": 999  # 6-of-a-kind
-	}
+func _ready():
+	RULES = JSON.parse_string(FileAccess.get_file_as_string("res://rules/base_rules.json"))
 
 func get_value(card: int): return card % RULES["VALS_PER_SUIT"]
 func get_suit(card: int): return card / RULES["VALS_PER_SUIT"]
@@ -77,27 +37,10 @@ func hand_to_string(hand: Array):
 	return hand_string
 
 func reset():
-	RULES = {
-		"SUITS": 4,
-		"VALS_PER_SUIT": 13,
-		"HOLE_CARDS": 2,
-		"COMM_CARDS": 5,
-		"CARDS_PER_HAND": 5,
-		"HAND_RANKS": {
-			# Base hands
-			"5K": 900, # 5-of-a-kind
-			"SF": 800, # Straight flush
-			"4K": 700, # 4-of-a-kind
-			"FH": 600, # Full house
-			"FL": 500, # Flush
-			"ST": 400, # Straight
-			"3K": 300, # 3-of-a-kind
-			"2P": 200, # 2 pair
-			"1P": 100, # 1 pair
-			"HC": 000, # High card
-			"": -9999, # Junk value
-		}
-	}
+	var basics = FileAccess.open("res://rules/base_rules.json", FileAccess.READ)
+	var as_text = basics.get_as_text()
+	basics.close()
+	RULES = JSON.parse_string(as_text)
 
 func set_rule(rule: StringName, value: Variant):
 	if RULES.has(rule) and typeof(RULES[rule]) == typeof(value):
@@ -107,11 +50,22 @@ func set_rule(rule: StringName, value: Variant):
 	# Special considerations for other rules
 	# e.g. setting/resetting certain hand ranks for certain hand sizes
 	if rule == "HAND_RANKS":
+		var card_rules
+		var path = "res://rules/%s.json"
+		
 		match value:
 			4:
-				pass
+				card_rules = load_json_at(path % "four_cards")
+				RULES["HAND_RANKS"].merge(card_rules, true)
 			5:
-				RULES["HAND_RANKS"] = BASE_HAND_RANKS
+				card_rules = load_json_at(path % "base_rules")
+				RULES["HAND_RANKS"] = card_rules["HAND_RANKS"]
 			6:
-				RULES["HAND_RANKS"].merge(SIX_HAND_RANKS, true)
-			_: push_error("wait you can't go that high")
+				card_rules = load_json_at(path % "six_cards")
+				RULES["HAND_RANKS"].merge(card_rules, true)
+			_: 
+				push_error("wait you can't have that many")
+
+func load_json_at(path: String):
+	var it = JSON.parse_string(FileAccess.get_file_as_string(path))
+	if it: return it
