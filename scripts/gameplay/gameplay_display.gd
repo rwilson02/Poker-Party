f60603246ca31e27f44d081c04e5d34928f3765c
rwilson02 <78097094+rwilson02@users.noreply.github.com>
2023-09-17 21:05:52 -0400

    chips icon

--- scripts/gameplay/gameplay_display.gd
+++ scripts/gameplay/gameplay_display.gd
@@ -2,7 +2,7 @@ extends Node
 
 @onready var HUD_self_text = $Scorebug/RichTextLabel
 
-const self_text_template = "[center]%s\nChips: %d[/center]"
+const self_text_template = "[center]%s\n[img=24]res://textures/ico_chips.png[/img] %d[/center]"
 
 func _ready():
 	Netgame.state_updated.connect(update_display)
